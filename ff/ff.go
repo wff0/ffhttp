231bd54e10e401455340b79b012b209e65f5841b
wff0 <1098137961@qq.com>
2024-05-23 17:05:03 +0800

    实现上下文

--- ff/ff.go
+++ ff/ff.go
@@ -1,25 +1,23 @@
 package ff
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(c *Context)
 
 type Engine struct {
-	route map[string]HandlerFunc
+	route *router
 }
 
 func New() *Engine {
 	return &Engine{
-		route: make(map[string]HandlerFunc),
+		route: newRoute(),
 	}
 }
 
-func (e *Engine) addRoute(method, path string, handlerFunc HandlerFunc) {
-	key := fmt.Sprintf("%s-%s", method, path)
-	e.route[key] = handlerFunc
+func (e *Engine) addRoute(method, path string, handler HandlerFunc) {
+	e.route.addRoute(method, path, handler)
 }
 
 func (e *Engine) GET(path string, handlerFunc HandlerFunc) {
@@ -35,10 +33,6 @@ func (e *Engine) Run(addr string) error {
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	key := fmt.Sprintf("%s-%s", r.Method, r.URL.Path)
-	if handler, ok := e.route[key]; ok {
-		handler(w, r)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
-	}
+	c := newContext(w, r)
+	e.route.handle(c)
 }
--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"ffhttp/ff"
-	"fmt"
 	"net/http"
 )
 
 func main() {
 	r := ff.New()
-	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL.Path: %q\n", r.URL.Path)
+	r.GET("/", func(c *ff.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello ff</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *ff.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *ff.Context) {
+		c.Json(http.StatusOK, ff.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
